2733271e9ecc854da825b2bcd39ab4d4f2192776
Xinyi Li <xinyil8023@gmail.com>
2024-02-15 15:38:48 -0800

    multi-page

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+const email1 = document.getElementById("email");
+const email2 = document.getElementById("emailConfirm");
+const errorMessage = document.getElementById("errorSpan");
+
 function confirmEmail() {
-  var email1 = document.getElementById("email");
-  var email2 = document.getElementById("emailConfirm");
-  var span = document.getElementById("errorSpan");
-  if (!email1 === email2) {
-    span.setAttribute("hidden", "hidden");
+  console.log(email1.nodeValue);
+  if (email1.value !== email2.value) {
+    errorMessage.hidden = false;
+  } else {
+    errorMessage.hidden = true;
   }
 }
+email1.addEventListener('change', confirmEmail);
+email2.addEventListener('change', confirmEmail);
+
+
